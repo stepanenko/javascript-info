b7bd24459783606da8c7294e954d08ff21a562d6
Serhiy Stepanenko <41634824+stepanenko@users.noreply.github.com>
2019-03-05 11:49:13 +0200

    Update symbol.js

--- symbol.js
+++ symbol.js
@@ -14,4 +14,6 @@ console.log(clone);    // { name: 'Serhiy', [Symbol(id)]: 'some id' }
 
 // We can only use strings or symbols as keys in objects.
 // Other types are converted to strings.
-
+let ex = {
+  0: 'test'    // same as "0": "test"
+}
