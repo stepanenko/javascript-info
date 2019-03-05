5ed5419754dc8d81ce9229d7b999d30978acd2df
Serhiy Stepanenko <41634824+stepanenko@users.noreply.github.com>
2019-03-05 11:46:08 +0200

    Update symbol.js

--- symbol.js
+++ symbol.js
@@ -6,6 +6,12 @@ let id = Symbol('id');
 
 user[id] = 'some id';
 
-console.log(user[id]);
+console.log(user[id]);    // some id
 
-// Symbols are skipped by for…in
+// Symbols are skipped by for…in, but:
+let clone = Object.assign({}, user);
+console.log(clone);    // { name: 'Serhiy', [Symbol(id)]: 'some id' }
+
+// We can only use strings or symbols as keys in objects.
+// Other types are converted to strings.
+
