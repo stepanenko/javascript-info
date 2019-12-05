3f1b802b8363f0523b8f7b6b340c43cd351397e0
stepanenko <serhiy.stepanenko@aiesec.net>
2019-12-05 19:13:41 +0200

    errors

--- README.md
+++ README.md
@@ -3,3 +3,7 @@
 https://javascript.info/
 
 How it's done now. From the basics to advanced topics with simple, but detailed explanations.
+
+Here we learn JavaScript, starting from scratch and go on to advanced concepts like OOP.
+
+We concentrate on the language itself here, with the minimum of environment-specific notes.
--- error_handling.js
+++ error_handling.js
@@ -1,2 +1,48 @@
 
-// Usually, a script “dies” (immediately stops) in case of an error, printing it to console.
+// Usually, a script “dies” (immediately stops) in case of an error, printing it to console. 
+// But we can use 'try..catch' that, instead of dying 'catches' errors...
+
+try {
+  console.log('start of try 1 runs...');
+  lalalala; // ...no errors here
+  console.log('end of try 1! never reached');
+
+} catch(err) {
+  console.log('An error has occured!');
+}
+
+
+try {
+  console.log('start of try 2 runs...');
+  // ...no errors here
+  console.log('end of try 2 runs...');
+
+} catch(err) {
+  console.log('Catch is ignored, no errors in "try"');
+}
+
+// try..catch only works for RUNTIME errors, the code must be runnable, 
+// it should be valid JavaScript
+// It won’t work if the code is syntactically wrong
+
+
+// try..catch works SYNCHRONOUSLY
+// If an exception happens in “scheduled” code, like in setTimeout, then try..catch won’t catch it
+// try {
+//   setTimeout(function() {
+//     noSuchVariable; // script will die here
+//   }, 1000);
+// } catch (e) {
+//   alert( "won't work" );
+// }
+
+
+// To catch an exception inside a scheduled function, try..catch must be inside that function:
+
+setTimeout(function() {
+  try {
+    noSuchVar;
+  } catch (err) {
+    console.log(`Error name: '${err.name}', error message: '${err.message}'`);
+  }
+}, 1000);
