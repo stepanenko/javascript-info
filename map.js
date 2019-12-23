2c471cda43ca4787ed63e003f1a6fb0c3f66d3bd
stepanenko <serhiy.stepanenko@aiesec.net>
2019-12-23 17:56:38 +0200

    map added

--- map.js
+++ map.js
@@ -7,4 +7,11 @@ map.set('ok', 234, 45);
 
 // Regular Object would convert keys to string but Map keeps the type
 
-console.log(map.keys());
+
+let john = { name: 'John' }
+
+let visitsMap = new Map();
+
+visitsMap.set(john, 23);
+
+console.log(visitsMap.get(john)); // 23
