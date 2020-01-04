23c12861c9e8f1de2d8635b942fa80c5485ffe7d
Serhiy Stepanenko <serhiy.stepanenko@aiesec.net>
2020-01-04 13:33:35 +0200

    class exploration

--- class.js
+++ class.js
@@ -3,7 +3,8 @@ class User {
   constructor(name) { this.name = name; }
   sayHi() { console.log(this.name); }
 }
-let jack = new User('Jack');
+let jack = new User.prototype.constructor('Jack'); // same as 'new User('Jack')'
+console.log(jack);
 
 // class is a function:
 console.log(typeof User); // function
@@ -18,6 +19,7 @@ console.log(User.prototype.sayHi); // [Function: sayHi]
 console.log(Object.getOwnPropertyNames(User.prototype)); // [ 'constructor', 'sayHi' ]
 
 // Class is not just a syntactic suger:
+
 // rewriting class User in pure functions:
 
 function User2(name) {
@@ -38,3 +40,25 @@ for (let prop in jack) {
 for (let prop in tom) {
   console.log(prop); // name, greet
 }
+
+// if a class expression has a name, it’s visible inside the class only:
+let User3 = class MyClass {
+  msg() {
+    console.log(MyClass);
+  }
+}
+
+new User3().msg(); // [Function: MyClass]
+// console.log(MyClass); error
+
+// we can create a class dynamically:
+function createClass(text) {
+  return class {
+    log() {
+      return console.log(text);
+    }
+  }
+}
+
+let myClass = createClass('Hello class');
+new myClass().log(); // Hello class
