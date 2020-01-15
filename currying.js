c186081834ad5628d30fe80eb7dd8bd0d559f3f7
stepanenko <serhiy.stepanenko@aiesec.net>
2020-01-15 13:43:36 +0200

    in process

---
+++ currying.js
@@ -0,0 +1,19 @@
+
+// currying doesn’t call a function. It just transforms it: f(a, b, c) -> f(a)(b)(c)
+
+function curry(f) { // curry(f) does the currying transform
+  return function(a) {
+    return function(b) {
+      return f(a, b);
+    };
+  };
+}
+
+// usage
+function sum(a, b) {
+  return a + b;
+}
+
+let curriedSum = curry(sum);
+
+console.log(curriedSum(1)(2)); // 3
