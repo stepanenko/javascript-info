d5928f8ee1bcfbcccef7d57d365a723f661f90ef
stepanenko <serhiy.stepanenko@aiesec.net>
2020-01-27 00:11:39 +0200

    fetch not finished

---
+++ fetch.js
@@ -0,0 +1,8 @@
+
+let promise = fetch(url, [options]);
+
+// fetch returns a promise
+// [options] - optional parameters: method, headers etc. if omitted will use GET request
+
+
+// continue at https://javascript.info/fetch
