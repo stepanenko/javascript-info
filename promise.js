ecb534395a64369be13f6ff6f3c751db4a606892
stepanenko <serhiy.stepanenko@aiesec.net>
2020-01-27 17:35:50 +0200

    promises review 1

--- promise.js
+++ promise.js
@@ -1,16 +1,62 @@
 
-let prom2 = new Promise((res, rej) => {
+// Resolved Promise:
+const promise1 = new Promise((resolve, reject) => {
   setTimeout(
-    () => res('done')
+    () => resolve('done')
   , 1000)
 });
+console.log('promise1:', promise1);   // promise1: Promise { <pending> }
 
-prom2.then(console.log);
-
-let promise = new Promise((res, rej) => {
-  // res('1');
-  rej('not found');
+let result;
+const status = promise1.then(function(data) {
+  result = data;
+  console.log('Inside then:', result);   // Inside then: done
+  return data;
 });
 
-promise.then(console.log, (e) => console.log('thats error: ' + e));
+console.log(status);   // Promise { <pending> }
+console.log('Outside then:', result);   // Outside then: undefined
 
+// Rejected Promise:
+const promise2 = new Promise((res, rej) => {
+  // res('1');
+  rej(new Error('Value not found'));
+});
+
+promise2.then(console.log, (error) => console.log('Error occured.', error.message));
+
+// ====== FROM ARTICLE ====== http://bit.ly/js_promise =====
+
+const myPromise = new Promise((resolve, reject) => {
+  if (Math.random() * 100 <= 90) {
+      resolve('Hello, Promises!');
+  }
+  reject(new Error('In 10% of the cases, I fail. Miserably.'));
+});
+
+// Two functions 
+const onResolved = (resolvedValue) => console.log(resolvedValue);
+const onRejected = (error) => console.log(error);
+
+myPromise.then(onResolved, onRejected);
+
+// Same as above, written concisely
+// myPromise.then((resolvedValue) => {
+//     console.log(resolvedValue);
+// }, (error) => {
+//     console.log(error);
+// });
+
+
+
+function prom() {
+  return new Promise((resolve, reject) => {
+    setTimeout(function() {
+      resolve('Success');
+     }, 2000);
+  });
+}
+
+const res = prom().then(data => {
+  console.log(data);
+});
