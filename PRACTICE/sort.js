084563ed85d070672db4f531f3113d5911bee34f
stepanenko <serhiy.stepanenko@aiesec.net>
2020-01-29 19:42:22 +0200

    added ternary approach

--- PRACTICE/sort.js
+++ PRACTICE/sort.js
@@ -1,11 +1,13 @@
 
 let array = [
   { rank: 2 },
+  { rank: 7 },
   { rank: 0 },
   { rank: -10 },
   { rank: NaN },
   { rank: 7 },
   { rank: 3 },
+  { rank: NaN },
   { rank: -1 }
 ];
 
@@ -19,7 +21,11 @@ let sorted = array.sort((a, b) => {
 
 });
 
-console.log(sorted);
+let sorted2 = array.sort((a, b) => {
+  return isNaN(a.rank) ? 1 : isNaN(b.rank) ? -1 : b.rank - a.rank;
+})
+
+console.log(sorted2);
 
 
 // From => https://obscurejavascript.tumblr.com/post/125103356483/sorts-and-nan-in-javascript
