bc59dae63c732c6cc89541ccab88fefcb4c82f21
stepanenko <serhiy.stepanenko@aiesec.net>
2020-03-08 12:20:37 +0200

    NaN work

---
+++ MDN/NaN.js
@@ -0,0 +1,20 @@
+
+function sanitise(x) {
+  if (isNaN(x)) {
+    return NaN;
+  }
+  return x;
+}
+
+console.log(sanitise('1'));
+// expected output: "1"
+
+console.log(sanitise('NotANumber'));
+// expected output: NaN
+
+// NaN, and only NaN, will compare unequal to itself:
+function valueIsNaN(v) {
+  return v !== v;
+}
+console.log(valueIsNaN(1));          // false
+console.log(valueIsNaN(NaN));        // true
