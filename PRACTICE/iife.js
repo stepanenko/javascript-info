58b3868f6a0ffa5c49f497294afb8aee805f7188
stepanenko <serhiy.stepanenko@aiesec.net>
2020-03-31 22:09:54 +0300

    iife

---
+++ PRACTICE/iife.js
@@ -0,0 +1,18 @@
+
+(function() {
+
+function foo(a, b) {
+    return bar(a, b);
+}
+
+function bar(a, b) {
+    return a + b;
+}
+
+console.log(foo(1, 2));   // 3
+
+}());
+
+// The self-executing function serves only to contain and protect the foo() and bar() functions,
+// but more importantly, the anonymous function executes only once —
+// thus the inner foo() and bar() functions are created only once.
