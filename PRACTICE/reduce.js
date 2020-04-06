40a0d372989bd04a10bdcc77a2408e67ac0eec4c
stepanenko <serhiy.stepanenko@aiesec.net>
2020-04-06 12:00:09 +0300

    reduce vs forEach

---
+++ PRACTICE/reduce.js
@@ -0,0 +1,28 @@
+
+const foo = {
+  foo: 1,
+  bar: 2,
+  baz: 3
+};
+
+let newObj = {};
+let myNewObj = {};
+
+Object.keys(foo).forEach(prop => newObj[prop] = null);
+let bar = Object.keys(foo).reduce((myNewObj, prop) => {
+  myNewObj[prop] = null;
+  return myNewObj;
+}, {});
+
+console.log(newObj);   // { foo: null, bar: null, baz: null }
+console.log(bar);      // { foo: null, bar: null, baz: null }
+
+
+const nums = [5, 9, 7];
+
+let check = nums.forEach((num, index) => nums[index] = num + 1);
+let added = nums.map(num => num + 1);
+
+console.log(check);   // undefined
+console.log(nums);    // [ 6, 10, 8 ]
+console.log(added);   // [ 7, 11, 9 ]
