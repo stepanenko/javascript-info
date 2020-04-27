a0af4f414f8bf907eaf3ae542e9bba5ef03fec5c
stepanenko <serhiy.stepanenko@aiesec.net>
2020-04-28 00:37:17 +0300

    tasks descroption added

--- INTERVIEW/test_elitex.js
+++ INTERVIEW/test_elitex.js
@@ -1,8 +1,13 @@
 
-// Tests from testdome.com: (duration 1 hour)
-// ===1 task (7 mins)
-function removeProperty(obj, prop) {
+// Tests from testdome.com (duration 1 hour)
 
+// === 1 task (7 mins) ===
+
+// Implement the removeProperty function which takes an object and property name, and does the following:
+// If the object obj has a property prop, the function removes the property from
+// the object and returns true; in all other cases it returns false.
+
+function removeProperty(obj, prop) {
   if (obj.hasOwnProperty(prop)) {
     delete obj[prop];
     return true;
@@ -10,12 +15,18 @@ function removeProperty(obj, prop) {
 }
 
 let obj = { name: 'ser '};
-// let output = removeProperty(obj, 'name');
+let output = removeProperty(obj, 'name');
 // console.log(output);
 // console.log(obj);
 
 
-// === 2 task (15 min)
+// === 2 task (15 mins) ===
+
+// Date: Write a function that converts user entered date formatted as M/D/YYYY
+// to a format required by an API (YYYYMMDD). 
+// The parameter "userDate" and the return value are strings.
+// For example, it should convert user entered date "12/31/2014" to "20141231" suitable for the API.
+
 function formatDate(userDate) {
   // format from M/D/YYYY to YYYYMMDD
   const year = new Date(userDate).getFullYear();
@@ -24,10 +35,14 @@ function formatDate(userDate) {
   return '' + year + month + date;
 }
 
-console.log(formatDate("1/31/2014"));
+console.log(formatDate("1/31/2014"));   // passed only 50%
 
 
 // === 3 task 20 mins (needs an HTML file testdome.html)
+
+// Closure: Fix the bugs in the registerHandlers function. An alert should display anchor's zero-based index within a document instead of following the link.
+// For example, in the document below, the alert should display "2" when Google anchor
+// is clicked since it is the third anchor element in the document and its zero-based index is 2.
 
 function registerHandlers() {
   var as = document.getElementsByTagName('a');
@@ -38,3 +53,6 @@ function registerHandlers() {
     }
   }
 }
+
+// You can get different certificates on their website
+// Tests screenshots and video are captured
