0ae3789c2e795df5700a4386fdc400791c1766ba
stepanenko <serhiy.stepanenko@aiesec.net>
2020-05-27 22:56:51 +0300

    play with map

--- JavaScript Info/map.js
+++ JavaScript Info/map.js
@@ -3,15 +3,20 @@ let map = new Map();
 
 map.set(1, 'one');
 map.set(2, 'two');
-map.set('ok', 234, 45);
+map.set('ok', 234, 45); // third arg is skipped
+// => Map { 1 => 'one', 2 => 'two', 'ok' => 234 }
 
 // Regular Object would convert keys to string but Map keeps the type
-
 
 let john = { name: 'John' }
 
 let visitsMap = new Map();
 
+visitsMap.set({ name: 'Bill', age: 33 }, 23);
+visitsMap.set('Sam', 65);
 visitsMap.set(john, 23);
+visitsMap.set(john, 34);  // gets overwritten
 
-console.log(visitsMap.get(john)); // 23
+let visitor = visitsMap.get('Sam');
+
+console.log(visitor);   // 65
