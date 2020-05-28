6638f6f8d7a0f468e598d560b7570c618261de06
stepanenko <serhiy.stepanenko@aiesec.net>
2020-05-28 22:48:46 +0300

    map more

--- JavaScript Info/map.js
+++ JavaScript Info/map.js
@@ -12,11 +12,46 @@ let john = { name: 'John' }
 
 let visitsMap = new Map();
 
-visitsMap.set({ name: 'Bill', age: 33 }, 23);
-visitsMap.set('Sam', 65);
-visitsMap.set(john, 23);
-visitsMap.set(john, 34);  // gets overwritten
+visitsMap.set({ name: 'Bill', age: 33 }, 23)
+  .set('Sam', 65)
+  .set(john, 23)
+  .set(john, 34);  // gets overwritten
 
-let visitor = visitsMap.get('Sam');
+let visitor = visitsMap.get('Sam');  // 65
 
-console.log(visitor);   // 65
+console.log(visitsMap);
+
+// Using objects as keys is one of most notable and important Map features.
+// For string keys, Object can be fine, but not for object keys.
+
+// For looping over a map, there are 3 methods:
+
+let recipeMap = new Map([
+  ['cucumber', 500],
+  ['tomatoes', 350],
+  ['onion', 50]
+]);
+
+// iterate over keys (vegetables)
+for (let vegetable of recipeMap.keys()) {
+  console.log(vegetable);   // cucumber, tomatoes, onion
+}
+
+// iterate over values (amounts)
+for (let amount of recipeMap.values()) {
+  console.log(amount);   // 500, 350, 50
+}
+
+// iterate over [key, value] entries
+for (let entry of recipeMap) { // the same as of recipeMap.entries()
+  console.log(entry);   // cucumber, 500 (and so on)
+}
+
+// Besides that, Map has a built-in forEach method, similar to Array:
+// runs the function for each (key, value) pair
+recipeMap.forEach((value, key, map) => {
+  console.log(`${key}: ${value}`);   // cucumber: 500 etc
+});
+
+// The iteration goes in the same order as the values were inserted.
+// Map preserves this order, unlike a regular Object.
