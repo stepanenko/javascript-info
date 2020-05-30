137b34b0bf02dda508a2d93c188b55644683f644
stepanenko <serhiy.stepanenko@aiesec.net>
2020-05-30 14:46:13 +0300

    set done

--- JavaScript Info/set.js
+++ JavaScript Info/set.js
@@ -18,10 +18,28 @@ set.add(mary);
 console.log(set.size);   // 3
 
 for (let user of set) {
-  console.log(user.name);   // John (then Pete and Mary)
+  console.log(user.name);   // John  Pete  Mary
 }
 
 // The alternative to Set could be an array of users, and the code to check for duplicates
 // on every insertion using arr.find. But the performance would be much worse,
 // because this method walks through the whole array checking every element.
 // Set is much better optimized internally for uniqueness checks.
+
+// __ Iteration __
+
+let carsSet = new Set(['BMW', 'Renault', 'Audi']);
+let fruits = new Set(['apples', 'oranges', 'kiwis']);
+
+for (const car of carsSet) console.log('for loop:', car);
+
+carsSet.forEach(el => console.log('forEach:', el));
+
+let keys = carsSet.keys();
+// => [Set Iterator] { 'BMW', 'Renault', 'Audi' }
+
+let values = carsSet.values();
+// => [Set Iterator] { 'BMW', 'Renault', 'Audi' }
+
+let entries = fruits.entries();
+// => [Set Iterator] { 'apples', 'oranges', 'kiwis' }
