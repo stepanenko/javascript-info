e5350c7a30fb49b9dba5b687cd18753d52577655
stepanenko <serhiy.stepanenko@aiesec.net>
2020-06-10 09:25:32 +0300

    rest vs spread

--- PRACTICE/destruct.js
+++ PRACTICE/destruct.js
@@ -1,18 +1,30 @@
 
-const obj = { name: 'John', age: 34 };
+const client = {
+  name: 'John',
+  age: 34
+};
 
-function getAge({age}) {
+function getAge({ age }) {
   console.log(age);
 }
+getAge(client);   // 34
 
-getAge(obj);   // 34
-
+const newFruits = {
+  bananas: { small: 5, big: 7},
+  oranges: 3
+};
 const fruitPrices = {
+  id: 1,
+  ...newFruits,   // on right side from '=' --> spread operator !! 
   apples: {
     green: 4,
     red: 2
   }
-}
+};
 
 const { apples: { red } } = fruitPrices;
 console.log(red);   // 2
+
+const { id, ...fruitsWithoutId } = fruitPrices;   // on left side from '=' --> rest operator !!
+console.log(id);   // 1
+console.log(fruitsWithoutId);   // { bananas: { small: 5, big: 7 }, oranges: 3, apples: { green: 4, red: 2 } } 
