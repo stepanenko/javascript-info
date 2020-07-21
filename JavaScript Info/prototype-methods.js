07c86490acb86a82c1fd0fbd6ab47260ed377983
stepanenko <serhiy.stepanenko@aiesec.net>
2020-07-22 00:29:24 +0300

    proto

--- JavaScript Info/prototype-methods.js
+++ JavaScript Info/prototype-methods.js
@@ -1,26 +1,21 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <title>Prototype Inheritance</title>
-</head>
-<body>
-<script>
+
 let animal = {
   eats: true,
   walk() {
     console.log('Animal walks')
   }
-}
+};
+
 let rabbit = {
   runs: true,
   __proto__: animal
-}
+};
 
 let longEar = {
   earlenght: 100,
   __proto__: rabbit
-}
+};
+
 //console.log(rabbit) // {runs: true}
 //console.log(rabbit.runs) // true
 //console.log(rabbit.eats) // true
@@ -28,7 +23,7 @@ let longEar = {
 //longEar.walk() //Animal walks\
 //console.log(longEar.runs) // true
 //console.log(longEar.eats) // true
- 
+
 // The prototype is only used for reading properties.
 
 let user = {
@@ -36,26 +31,26 @@ let user = {
   surname: 'Doe',
 
   get fullName() {
-    return `${this.name} ${this.surname}`
+    return `${this.name} ${this.surname}`;
   },
 
   set fullName(val) {
-    [this.name, this.surname] = val.split(' ')
+    [this.name, this.surname] = val.split(' ');
   }
-}
+};
 
 let admin = {
   isAdmin: true,
   __proto__: user
-}
+};
 
-//console.log(admin.fullName) // John Doe
-admin.fullName = 'Vik Steps'
-//console.log(admin.fullName) // Vik Steps
-//console.log(admin.isAdmin) // true
+// console.log(admin.fullName)   // John Doe
+admin.fullName = 'Vik Steps';
+// console.log(admin.fullName)   // Vik Steps
+// console.log(admin.isAdmin)   // true
 
-//console.log(admin.__proto__) //{name: "John", surname: "Doe"}
-//console.log(admin.__proto__ == user) // true
+// console.log(admin.__proto__)   //{name: "John", surname: "Doe"}
+// console.log(admin.__proto__ == user)   // true
 
 
 // === TASKS ====
@@ -63,17 +58,14 @@ admin.fullName = 'Vik Steps'
 let anim = {
   jumps: null
 };
+
 let rabb = {
   __proto__: anim,
   jumps: true
 };
 
-console.log( rabb.jumps ); // ? (1)
+console.log(rabb.jumps); // ? (1)
 delete rabb.jumps;
-console.log( rabb.jumps ); // ? (2)
+console.log(rabb.jumps); // ? (2)
 delete anim.jumps;
-console.log( rabb.jumps ); // ? (3)
-
-</script>
-</body>
-</html>
+console.log(rabb.jumps); // ? (3)
