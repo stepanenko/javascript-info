07f36c879a8e6950379d009eed91b6f9cfb6a668
stepanenko <serhiy.stepanenko@aiesec.net>
2020-08-12 16:08:05 +0300

    example with weak map

--- JavaScript Info/weak_map.js
+++ JavaScript Info/weak_map.js
@@ -10,14 +10,14 @@ weak.set(obj, "ok");   // works fine (object key)
 weak.set(john, "good");   // works fine (object key)
 weak.set({ name: 'Larry' }, "ok");
 
-// unlike map, wekamap can't use a string as the key
+// unlike map, weakmap can't use a string as the key
 // weak.set("test", "Whoops");   // TypeError: Invalid value used as weak map key
 
 console.log(weak);   // WeakMap { <items unknown> }
 console.log(weak.get(obj));   // ok
 console.log(weak.get({ name: 'Larry' }));   // undefined
 // If an object has lost all other references, then it is to be garbage-collected automatically
-john = null;   // or 'john = {}' ...
+john = {};   // or 'john = null' ...
 console.log(weak.get(john));   // undefined
 
 // WeakMap has only the following methods:
@@ -28,3 +28,19 @@ console.log(weak.get(john));   // undefined
 
 
 // Now where do we need such data structure? ... https://javascript.info/weakmap-weakset
+// ##### 1. Additional Data Storage: #####
+weak.set(john, "secret documents");
+console.log(weak.get(john));   // secret documents
+// if john dies, secret documents will be destroyed automatically
+let visitsCountMap = new WeakMap();
+function countUser(user) {
+  let count = visitsCountMap.get(user) || 0;
+  visitsCountMap.set(user, count + 1);
+}
+let jack = { name: 'Jack' };
+countUser(jack);
+console.log(visitsCountMap.get(jack));   // 1
+// After john object becomes unreachable by all means except as a key of WeakMap,
+// it gets removed from memory, along with the information by that key from WeakMap.
+
+// ##### 2. Caching #####
