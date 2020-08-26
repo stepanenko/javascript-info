5ddb411876a0eaea8169eb9ae9db2b0197bc2c2c
stepanenko <serhiy.stepanenko@aiesec.net>
2020-08-26 20:28:56 +0300

    testing continued

--- Testing/cats.test.js
+++ Testing/cats.test.js
@@ -1,14 +1,21 @@
 
 const rewire = require('rewire');
-const expect = require('chai').expect;
+const expect = require('chai').expect; // not using assert and should, only expect
 const sinon = require('sinon');
 
 const cats = require('./cats');
-const wired = rewire('./cats.js');
+const rewired = rewire('./cats.js');
 
-describe('Cats File:', () => {
+describe('Tests for Cats File:', () => {
 
   describe('Public createCat function', () => {
+    beforeEach(() => {
+      
+    });
+    afterEach(() => {
+      
+    });
+
     it('should return an object', () => {
       let result = cats.createCat('Junny');
       expect(result).to.be.an('object');
@@ -16,8 +23,17 @@ describe('Cats File:', () => {
   });
 
   describe('Private createFunnyName function', () => {
-    let privateFunc = wired.__get__('createFunnyName');
+    function replace(nickname) {
+      return `<<${nickname}>>`;
+    }
+
+    let privateFunc = rewired.__get__('createFunnyName');
+    sinon.stub(rewired, 'createFunnyName').returns('555');
+    // let result = privateFunc('kit');
+    let prv = rewired.__set__('createFunnyName', replace);
+
     let result = privateFunc('kit');
+    console.log('privateFunc', privateFunc(5));
 
     it('should return a string', () => {
       expect(result).to.be.string;
@@ -27,4 +43,24 @@ describe('Cats File:', () => {
       expect(result).to.be.equal('^^kit^^');
     });
   });
+});
+
+
+
+
+// Hooks in Mocha:
+describe('collection of individual test spec', function () {
+  before(function () {
+    // runs before all tests in this block
+  });
+  after(function () {
+    // runs after all tests in this block
+  });
+  beforeEach(function () {
+    // runs before each test in this block
+  });
+  afterEach(function () {
+    // runs after each test in this block
+  });
+  // test cases
 });
