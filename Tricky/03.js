6d8d26115dfe6889ddd9156a153a5ce031154078
Serhiy Stepanenko <serhiy.stepanenko@aiesec.net>
2020-09-20 00:02:05 +0300

    new file

---
+++ Tricky/03.js
@@ -0,0 +1,13 @@
+
+var age = 33;
+// console.log(this.age);  // ?
+// console.log(self.age);  // ?
+
+let city = 'London';
+// console.log(this.city);  // ?
+// console.log(self.city);  // ?
+
+// this === self  // ?
+
+console.log(this);  // ? in node / browser
+console.log(self);  // ? in node / browser
