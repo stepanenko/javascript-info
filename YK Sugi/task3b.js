0bdd328c6eb5cb320d181dbad0ea41c0b1f7c5f3
stepanenko <serhiy.stepanenko@aiesec.net>
2020-11-28 12:41:32 +0200

    updated

--- YK Sugi/task3b.js
+++ YK Sugi/task3b.js
@@ -1,20 +1,34 @@
 
 // Calculate the sum of diagonal numbers
 let arr = [
-  [4, 2, 3],
+  [1, 2, 3],
   [4, 5, 6],
   [7, 8, 9]
 ];
 
-let sum = 0;
-let index = 0;
+function diaSum2(arr) {
+  let sum = 0;
+  let index = 0;
 
-arr.forEach(row => {
-  row.forEach((elem, elemIndex) => {
-    if (elemIndex === index)
-      sum += row[index];
+  arr.forEach(row => {
+    row.forEach((elem, elemIndex) => {
+      if (elemIndex === index)
+        sum += row[index];
+    });
+    index++;
   });
-  index++;
-});
 
-console.log('sum', sum);
+  return sum;
+}
+
+// Teacher solution:
+function diaSum(arr) {
+  let sum = 0;
+  for (let i = 0; i < arr.length; i++) {
+    sum += arr[i][i];
+  }
+  return sum;
+}
+
+let res = diaSum(arr);
+console.log('sum', res);
