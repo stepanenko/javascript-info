df29be9c4b08920f5cc8ee87abf13ff12a5aaa13
Serhiy Stepanenko <serhiy.stepanenko@aiesec.net>
2020-12-12 22:55:15 +0200

    edited main page

--- Redux/Redux React App/src/App.js
+++ Redux/Redux React App/src/App.js
@@ -1,22 +1,14 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Hello React App</h1>
+        <p>I would like to add Redux here. From Mosh Redux course.</p>
+        <p>I will use Twitter API to add my tweets to the store.</p>
+        <p>Maybe add SASS and Typescript later</p>
       </header>
     </div>
   );
