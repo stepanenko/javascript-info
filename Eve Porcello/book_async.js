ad6bb5f1ff96de0960d398892d150e49e89dad2e
Serhiy Stepanenko <41634824+stepanenko@users.noreply.github.com>
2021-01-01 14:13:24 +0200

    Update book_async.js

--- Eve Porcello/book_async.js
+++ Eve Porcello/book_async.js
@@ -22,3 +22,22 @@ const getFakePerson = async () => {
 getFakePerson();
 // Instead of waiting for the results of a promise to resolve and handling it with a chain of then functions,
 // async functions can be told to wait for the promise to resolve before further executing any code found in the function.
+
+// page 40:
+
+const getPeople = count =>
+  new Promise((resolves, rejects) => {
+    const api = `https://api.randomuser.me/?nat=US&results=${count}`;
+    const request = new XMLHttpRequest();
+    request.open("GET", api);
+    request.onload = () =>
+      request.status === 200
+        ? resolves(JSON.parse(request.response).results)
+        : reject(Error(request.statusText));
+    request.onerror = err => rejects(err);
+    request.send();
+  });
+
+getPeople(5)
+  .then(members => console.log(members))
+  .catch(error => console.error(`getPeople failed: ${error.message}`));
