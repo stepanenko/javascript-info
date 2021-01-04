e82047295abe81f025d0f7d4d24727442a17d259
Serhiy Stepanenko <41634824+stepanenko@users.noreply.github.com>
2021-01-04 17:13:06 +0200

    Create book_Imper-vs-Decl.js

---
+++ Eve Porcello/book_Imper-vs-Decl.js
@@ -0,0 +1,29 @@
+
+// Functional programming is a part of a larger programming paradigm: declarative programming.
+// Declarative programming is a style of programming where applications are structured in a way
+// that prioritizes describing what should happen over defining how it should happen.
+
+// Imperative example:
+
+const string = "Restaurants in Hanalei";
+let urlFriendly = "";
+
+for (let i = 0; i < string.length; i++) {
+  if (string[i] === " ") {
+    urlFriendly += "-";
+  } else {
+    urlFriendly += string[i];
+  }
+}
+
+console.log(urlFriendly); // "Restaurants-in-Hanalei"
+
+// Just looking at the code above does not tell us much.
+// Imperative programs require lots of comments in order to understand what’s going on.
+
+// Now the Declarative approach to the same task:
+
+const string2 = "Restaurants in Hanalei";
+const urlFriendly2 = string.replace(/ /g, "-");
+
+console.log(urlFriendly2); // "Restaurants-in-Hanalei"
