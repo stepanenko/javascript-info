378e865977b266b3f6ac04a6598ca306eb52ce08
Serhiy Stepanenko <41634824+stepanenko@users.noreply.github.com>
2021-01-05 16:00:29 +0200

    Create book_functional-concepts.js

---
+++ Eve Porcello/book_functional-concepts.js
@@ -0,0 +1,8 @@
+
+// page 54
+
+// When writing functions, try to follow these three rules:
+// - 1. The function should take in at least one argument
+// - 2. The function should return a value or another function
+// - 3. The function should not change or mutate any of its arguments
+
