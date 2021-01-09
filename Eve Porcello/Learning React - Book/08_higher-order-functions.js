96090fdc766112ea4f130a265bbe8f289c602c72
stepanenko <serhiy.stepanenko@aiesec.net>
2021-01-09 12:46:11 +0200

    higher

---
+++ Eve Porcello/Learning React - Book/08_higher-order-functions.js
@@ -0,0 +1,19 @@
+// page 61
+
+// Higher-order functions are functions that can manipulate other functions.
+// They can take functions in as arguments or return functions or both.
+
+// The first category of higher-order functions are functions that expect other functions as arguments:
+// Array.map, Array.filter, and Array.reduce
+
+// Example:
+
+const invokeIf = (condition, fnTrue, fnFalse) =>
+  condition ? fnTrue() : fnFalse();
+
+const showWelcome = () => console.log("Welcome!!!");
+
+const showUnauthorized = () => console.log("Unauthorized!!!");
+
+invokeIf(true, showWelcome, showUnauthorized); // "Welcome!!!"
+invokeIf(false, showWelcome, showUnauthorized); // "Unauthorized!!!"
