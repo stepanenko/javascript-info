a906b6109a43102a213db74c08a466dfce607c1b
Serhiy Stepanenko <41634824+stepanenko@users.noreply.github.com>
2021-01-14 22:13:41 +0200

    Update 08_higher-order-functions.js

--- Eve Porcello/Learning React - Book/08_higher-order-functions.js
+++ Eve Porcello/Learning React - Book/08_higher-order-functions.js
@@ -1,10 +1,11 @@
+
 // page 61
 
 // Higher-order functions are functions that can manipulate other functions.
 // They can take functions in as arguments or return functions or both.
 
 // The first category of higher-order functions are functions that expect other functions as arguments:
-// Array.map, Array.filter, and Array.reduce
+// e.g.: Array.map, Array.filter or Array.reduce
 
 // Example:
 
