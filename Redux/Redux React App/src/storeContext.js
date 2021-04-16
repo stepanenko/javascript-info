75023e5f4ffe0fd9b4c458cb4e2a0056ccd35a36
Serhiy Stepanenko <serhiy.stepanenko@aiesec.net>
2021-04-16 20:27:12 +0300

    removed redux

--- Redux/Redux React App/src/storeContext.js
+++
@@ -1,6 +0,0 @@
-
-import { createContext } from 'react';
-
-const StoreContext = createContext();
-
-export default StoreContext;
--- Redux/Redux webpack App/src/components/Button.js
+++
@@ -1,7 +0,0 @@
-
-export const CounterButton = (title, doing) => {
-  const btn = document.createElement('button');
-  btn.innerHTML = title;
-  btn.addEventListener('click', doing);
-  return btn;
-};
