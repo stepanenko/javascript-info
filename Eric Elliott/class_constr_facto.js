d53388af0c2b41607612e0cfcadf10d8100972d3
stepanenko <serhiy.stepanenko@aiesec.net>
2021-06-17 00:15:30 +0300

    new work

---
+++ Eric Elliott/class_constr_facto.js
@@ -0,0 +1,51 @@
+
+// class
+class ClassCar {
+    drive() {
+        console.log('Vroom!');
+    }
+}
+
+const car1 = new ClassCar();
+console.log(car1.drive());
+
+
+// constructor
+function ConstructorCar() { }
+
+ConstructorCar.prototype.drive = function () {
+    console.log('Vroom!');
+};
+
+const car2 = new ConstructorCar();
+console.log(car2.drive());
+
+
+// factory
+const proto = {
+    drive() {
+        console.log('Vroom!');
+    }
+};
+
+const factoryCar = () => Object.create(proto);
+
+const car3 = factoryCar();
+console.log(car3.drive());
+
+// Each of these strategies stores methods on a shared prototype, and optionally supports
+// private data via constructor function closures. In other words, they have mostly the same features,
+// and could mostly be used interchangeably.
+
+// In JavaScript, any function can return a new object. When it’s not a constructor function or class,
+// it’s called a factory function.
+
+// ES6 classes desugar to constructor functions, so everything that follows about constructor functions
+// also applies to ES6 classes:
+
+class Foo {}
+console.log(typeof Foo); // function
+
+
+
+// https://medium.com/javascript-scene/javascript-factory-functions-vs-constructor-functions-vs-classes-2f22ceddf33e
--- JavaScript Info/currying.js
+++ JavaScript Info/currying.js
@@ -1,9 +1,11 @@
 
-// currying doesn’t call a function. It just transforms it: f(a, b, c) -> f(a)(b)(c)
+// Currying is a transformation of functions that translates
+// a function from callable as f(a, b, c) into callable as f(a)(b)(c).
+// Currying doesn’t call a function. It just transforms it: f(a, b, c) -> f(a)(b)(c)
 
-function curry(f) { // curry(f) does the currying transform
-  return function(a) {
-    return function(b) {
+function curry1(f) { // curry(f) does the currying transform
+  return function (a) {
+    return function (b) {
       return f(a, b);
     };
   };
@@ -14,7 +16,57 @@ function sum(a, b) {
   return a + b;
 }
 
-let curriedSum = curry(sum);
+let curriedSum = curry1(sum);
 
-console.log(curriedSum(1)(2)); // 3
+console.log(curriedSum(1)(2)); // 3, called partially
 
+
+// More advanced implementations of currying, such as _.curry from lodash library,
+// return a wrapper that allows a function to be called both normally and partially:
+const _ = require('lodash');
+
+curriedSum = _.curry(sum);
+
+console.log(curriedSum(1, 2)); // 3, still callable normally, requires Lodash
+
+
+// For instance, we have the logging function log(date, imp, msg) that formats and outputs
+// the information. In real projects such functions have many useful features like sending
+// logs over the network, here we’ll just use alert:
+
+function log(date, importance, message) {
+  console.log(`[${date.getHours()}:${date.getMinutes()}] [${importance}] ${message}`);
+}
+
+// Let’s curry it!
+
+log = _.curry(log); // requires Lodash
+
+// After that log works normally:
+
+log(new Date(), "DEBUG", "some debug 1"); // log(a, b, c)
+
+// But also works in the curried form:
+
+log(new Date())("DEBUG")("some debug 2"); // log(a)(b)(c)
+
+// Now we can easily make a convenience function for current logs:
+
+// logNow will be the partial of log with fixed first argument
+let logNow = log(new Date());
+
+// use it
+logNow("INFO", "message"); // [HH:mm] INFO message
+
+// Now logNow is log with fixed first argument,
+// in other words “partially applied function” or “partial” for short.
+
+// We can go further and make a convenience function for current debug logs:
+
+let debugNow = logNow("DEBUG");
+
+debugNow("message"); // [HH:mm] DEBUG message
+
+// So, the benefits are:
+// We didn’t lose anything after currying: log is still callable normally.
+// We can easily generate partial functions such as for today’s logs.
