41cc2dc1b5c87f6f8a78a331fba20fb2e6da2467
Serhiy Stepanenko <41634824+stepanenko@users.noreply.github.com>
2021-07-17 12:56:17 +0300

    Update async_loop.js

--- PRACTICE/NewFire/async_loop.js
+++ PRACTICE/NewFire/async_loop.js
@@ -30,7 +30,7 @@ async function printFiles() {
     });
 }
 
-printFiles();
+// printFiles();
 
 // Res:
 // file url: contents...
@@ -39,3 +39,49 @@ printFiles();
 // Time taken in milliseconds: 2015
 // file url 3: contents...
 // Time taken in milliseconds: 2015
+
+// FOR ... OF
+async function printFiles2() {
+    let startTime2 = Date.now();
+    const files = await getFilePaths();
+
+    for (const file of files) {
+        const contents = await readFile(file);
+        console.log(contents);
+        let finishTime = Date.now();
+        let timeTaken = finishTime - startTime2;
+        console.log("Time taken in milliseconds: " + timeTaken);
+    }
+}
+
+// printFiles2();
+// res:
+// file url: contents...
+// Time taken in milliseconds: 2025
+// file url 2: contents...
+// Time taken in milliseconds: 3036
+// file url 3: contents...
+// Time taken in milliseconds: 4041
+
+
+// Async Iteration
+// FOR AWAIT ... OF
+async function printFiles3() {
+    let startTime2 = Date.now();
+    const files = await getFilePaths()
+
+    const promises = files.map(file => readFile(file)); // promises: [ Promise { <pending> }, Promise { <pending> }, Promise { <pending> } ]
+
+    // for-await-of converts each iterated value via Promise.resolve() to a Promise, which it then awaits.
+    // That means that it works for both Promises and normal values.
+    for await (const contents of promises) {
+        console.log(contents);
+        let finishTime = Date.now();
+        let timeTaken = finishTime - startTime2;
+        console.log("Time taken in milliseconds: " + timeTaken);
+    }
+}
+
+printFiles3();
+
+// more: https://2ality.com/2016/10/asynchronous-iteration.html
