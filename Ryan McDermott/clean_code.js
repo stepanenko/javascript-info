b85d388b7cfd86d20f7f772163e9c656102ebda0
Serhiy Stepanenko <41634824+stepanenko@users.noreply.github.com>
2021-11-19 12:55:00 +0200

    Create clean_code.js

---
+++ Ryan McDermott/clean_code.js
@@ -0,0 +1,36 @@
+
+// Make objects have private members
+// This can be accomplished through closures (for ES5 and below).
+
+// ==== Bad: ====
+
+const Employee = function(name) {
+  this.name = name;
+};
+
+Employee.prototype.getName = function getName() {
+  return this.name;
+};
+
+const employee = new Employee("John Doe");
+console.log(`Employee name: ${employee.getName()}`); // Employee name: John Doe
+delete employee.name;
+console.log(`Employee name: ${employee.getName()}`); // Employee name: undefined
+
+
+// ==== Good: ====
+
+function makeEmployee(name) {
+  return {
+    getName() {
+      return name;
+    }
+  };
+}
+
+const employee = makeEmployee("John Doe");
+console.log(`Employee name: ${employee.getName()}`); // Employee name: John Doe
+delete employee.name;
+console.log(`Employee name: ${employee.getName()}`); // Employee name: John Doe
+
+// More at: https://github.com/ryanmcdermott/clean-code-javascript
