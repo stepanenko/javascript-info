ab74b358976eab91c0ab57645194603cfeba0d87
Serhiy Stepanenko <41634824+stepanenko@users.noreply.github.com>
2024-05-08 21:03:47 +0300

    Update singleton.js

--- Lydia Hallie/singleton.js
+++ Lydia Hallie/singleton.js
@@ -1,6 +1,6 @@
-// The Object.freeze method makes sure that consuming code cannot
-// modify the Singleton. Properties on the frozen instance cannot be added or
-// modified, which reduces the risk of accidentally overwriting the values on the Singleton.
+// Singletons are classes that can be instantiated once and can be accessed
+// globally. This single instance can be shared throughout our application, which
+// makes Singletons great for managing global state in an application.
 
 let instance;
 let counter = 0;
