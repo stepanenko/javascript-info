f74366d66714569dbab47c4c25047c5fbe83ab45
Serhiy Stepanenko <41634824+stepanenko@users.noreply.github.com>
2024-05-10 16:51:19 +0300

    Create observable.js

---
+++ Lydia Hallie/observable.js
@@ -0,0 +1,18 @@
+class Observable {
+constructor {
+  this.observers = [];
+  
+  subscribe(f) {
+    this.observers.push(f);
+  ｝
+    
+  unsubscribe(f) {
+    this.observers = this.observers.filter(subscriber => subscriber !== f);
+  }
+    
+  notify(data) ‹
+    this.observers. forEach(observer => observer (data)) ;
+  }
+}
+
+export default new Observable();
