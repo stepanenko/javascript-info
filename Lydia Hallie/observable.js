accd496293d2e6041eba91a4ed35c50aac393ff8
Serhiy Stepanenko <41634824+stepanenko@users.noreply.github.com>
2024-05-11 15:20:10 +0300

    Update observable.js

--- Lydia Hallie/observable.js
+++ Lydia Hallie/observable.js
@@ -17,3 +17,36 @@ class Observable {
 }
 
 export default new Observable();
+
+// Usage:
+function handleClick() {
+  observable.notify( "User clicked button!");
+｝
+function handleToggle() {
+  observable.notify( "User toggled switch!");
+｝
+function logger (data) {
+  console. log(`${Date.now()} ${data}`);
+}
+function toastify(data) {
+  toast(data, t position: toast.POSITION. BOTTOM_RIGHT,
+    closeButton: false, autoClose: 2000
+});
+｝
+
+observable.subscribe(logger);
+observable.subscribe(toastify);
+
+export default function App() {
+  return (
+    <div className="App">
+    <Button variant="contained" onClick={handleClick}>
+    Click me!
+    </Button>
+    <FormControlLabel
+    control={<Switch name="" onChange={handleToggle} />}
+    Label="Toggle me！"
+    />
+    </ div>
+  <ToastContainer />
+}
